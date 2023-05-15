7b9b2011e19bdecfa2770d3dfa602f1f731c80c4
KP1709 <63492101+KP1709@users.noreply.github.com>
2023-05-15 11:15:41 +0100

    Added updateCard function + move submit function here
    Added submit function here
    UpdateCard() only allows the card to be submitted if there is a score given by the user.

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+let questionCard = document.getElementById('question-state')
+let submittedCard = document.getElementById('submitted-state')
+
 const btnElScore = document.querySelectorAll(".feedback-scr-btn");
 const selectedScoreEl = document.querySelector("#selected-score");
+const submitBtn = document.querySelector(".submit-btn");
+
+document.querySelector(".submit-btn").addEventListener('click', updateCard)
 
 let score = ""
 
-btnElScore.forEach(btnEl =>{
-  btnEl.addEventListener('click', () =>{
+btnElScore.forEach(btnEl => {
+  btnEl.addEventListener('click', () => {
     document.querySelector('.active')?.classList.remove('active');
     btnEl.classList.add('active');
     score = btnEl.innerText
-    selectedScoreEl.innerText= `You selected ${score} out of 5`  
   });
 });
+
+// User must enter a score before submitting
+function updateCard() {
+  if (score != "") {
+    document.querySelector('#submitted-state').style.display = "flex";
+    selectedScoreEl.innerText = `You selected ${score} out of 5`
+    document.querySelector('#question-state').style.display = "none";
+  }
+}
+
